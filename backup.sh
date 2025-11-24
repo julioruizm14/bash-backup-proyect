dc9c4c6c2ab1b25f9025ad7c57070dacd65fd905
Julio David Ruiz Mendoza <julioruizm14@gmail.com>
2025-11-24 16:58:41 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -15,18 +15,15 @@ then
   exit
 fi
 
-# [TASK 1]
+
 targetDirectory=$1
 destinationDirectory=$2
 
-# [TASK 2]
 echo "Target directory is: $targetDirectory"
 echo "Destination directory is: $destinationDirectory"
 
-# [TASK 3]
 currentTS=$(date +%s)
 
-# [TASK 4]
 backupFileName="backup-[$currentTS].tar.gz"
 
 # We're going to:
@@ -36,37 +33,25 @@ backupFileName="backup-[$currentTS].tar.gz"
 
 # To make things easier, we will define some useful variables...
 
-# [TASK 5]
 origAbsPath=`pwd`
 
-# [TASK 6]
 cd "$destinationDirectory" || exit
 destAbsPath=`pwd`
 
-# [TASK 7]
 cd "$origAbsPath" || exit
 cd "$targetDirectory" || exit
 
-# [TASK 8]
 yesterdayTS=$(($currentTS - 24 * 60 * 60))
 
 declare -a toBackup
 
-for file in *  # [TASK 9]
+for file in *  
 do
-  # [TASK 10]
   if [[ `date -r "$file" +%s` -gt $yesterdayTS ]]
   then
-    # [TASK 11]
     toBackup+=("$file")
   fi
 done
 
-# [TASK 12]
 tar -czvf "$backupFileName" "${toBackup[@]}"
-
-# [TASK 13]
 mv "$backupFileName" "$destAbsPath"
-
-
-# Congratulations! You completed the final project for this course!
